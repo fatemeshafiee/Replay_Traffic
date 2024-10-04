fa9a9f1fc301eae99937a8221bda2ef4b8e59617
Fatemeh Shafiei Ardestani <61053360+fatemeshafiee@users.noreply.github.com>
2024-10-03 21:33:57 -0400

    Update client_to_server.py

--- client_to_server.py
+++ client_to_server.py
@@ -25,7 +25,8 @@ class Replay_Traffic(Thread):
         last_timestamp = None
         for pkt in reader:
             pkt_count += 1
-            if IP in pkt and new_pkt[IP].src == oldSource:
+            # and new_pkt[IP].src == oldSource
+            if IP in pkt :
                 new_pkt = pkt.copy()
                 new_pkt[IP].dst = destinationIP
                 new_pkt[IP].src = sourceIP
@@ -49,54 +50,46 @@ class Replay_Traffic(Thread):
         print(f"Total packets sent: {pkt_count}")
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print("Usage: python traffic_replay_frag.py config.txt")
         sys.exit(1)
 
-    config_path = sys.argv[1]
+    sourceIP = sys.argv[1]
+    destinationIP = sys.argv[2]
     threads = [] #added later
 
-    with open(config_path, "r") as f:
-        for line in f:
-            line = line.strip()
-            if not line or line.startswith("#"):
+
+    try:
+        ifname = "gtp-gnb"
+        addr = ni.ifaddresses(ifname)
+        sourceIP = "12.1.1.2"
+        print(sourceIP)
+        if sourceIP in ["12.1.1.2", "12.1.1.3", "12.1.1.4"]:
+            pcap_dir = "../ddos-data-sets-2022/benign_traffic"
+            pcap_files = os.listdir(os.path.join(pcap_dir))
+            if not pcap_files:
+                print(f"No pcap files found for traffic type. Skipping...")
+                continue
+        else: 
+            pcap_dir = "../malicious"
+            pcap_files = os.listdir(os.path.join(pcap_dir))
+            if not pcap_files:
+                print(f"No pcap files found for traffic type. Skipping...")
                 continue
 
-            parts = line.split()
-            if len(parts) == 3:
-                sourceIP, destinationIP, oldSource = parts
-
-            try:
-                ifname = "gtp-gnb"
-                addr = ni.ifaddresses(ifname)
-                sourceIP = "12.1.1.2"
-                print(sourceIP)
-                if sourceIP in ["12.1.1.2", "12.1.1.3", "12.1.1.4"]:
-                    pcap_dir = "../ddos-data-sets-2022/benign_traffic"
-                    pcap_files = os.listdir(os.path.join(pcap_dir))
-                    if not pcap_files:
-                        print(f"No pcap files found for traffic type. Skipping...")
-                        continue
-                else: 
-                    pcap_dir = "../malicious"
-                    pcap_files = os.listdir(os.path.join(pcap_dir))
-                    if not pcap_files:
-                        print(f"No pcap files found for traffic type. Skipping...")
-                        continue
 
 
+    except:
+        pass
+    #exit()
 
-            except:
-                pass
-            #exit()
-
-            for pcap_file in pcap_files:
-                
-                full_pcap_path = os.path.join(pcap_dir, pcap_file)
-                print(full_pcap_path)
-                traffic_generator = Replay_Traffic(sourceIP, destinationIP,oldSource,full_pcap_path)
-                traffic_generator.send_packet()
-                threads.append(traffic_generator)
+        for pcap_file in pcap_files:
+            
+            full_pcap_path = os.path.join(pcap_dir, pcap_file)
+            print(full_pcap_path)
+            traffic_generator = Replay_Traffic(sourceIP, destinationIP,oldSource,full_pcap_path)
+            traffic_generator.send_packet()
+            threads.append(traffic_generator)
 
     # Wait for all threads to complete
     for thread in threads:
