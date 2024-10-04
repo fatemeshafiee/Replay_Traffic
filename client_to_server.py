5bcefc2146f14ff9cbc836ac183f72c410b83b4f
Fatemeh Shafiei Ardestani <61053360+fatemeshafiee@users.noreply.github.com>
2024-10-03 22:14:55 -0400

    Update client_to_server.py

--- client_to_server.py
+++ client_to_server.py
@@ -9,16 +9,16 @@ import netifaces as ni
 
 
 class Replay_Traffic(Thread):
-    def __init__(self,sourceIP, destinationIP, oldSource, pcap_file):
+    def __init__(self,sourceIP, destinationIP, pcap_file):
         Thread.__init__(self)
         self.sourceIP = sourceIP
         self.destinationIP = destinationIP
-        self.oldSource = oldSource
+        print("create the class")
         # self.sourcePort = sourcePort
         # self.destinationPort = destinationPort
         self.pcap_file = pcap_file
-    def send_packet(self,sourceIP, destinationIP, oldSource, pcap_file):
-
+    def send_packet(self,sourceIP, destinationIP, pcap_file):
+        print("in the send packet!!!")
         reader = PcapReader(pcap_file)
         pkt_count = 0
 
@@ -46,50 +46,47 @@ class Replay_Traffic(Thread):
                 fragments = fragment(new_pkt, fragsize=1500)
                 for frag in fragments:
                     sendp(frag, iface="eth1",verbose=False)
+                    print("sent a packet")
 
         print(f"Total packets sent: {pkt_count}")
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Usage: python traffic_replay_frag.py config.txt")
+    if len(sys.argv) != 4:
+        print("Usage: python traffic_replay_frag.py sourceIP destinationIP")
         sys.exit(1)
 
     sourceIP = sys.argv[1]
     destinationIP = sys.argv[2]
+    is_attack = sys.argv[3]
     threads = [] #added later
 
 
     try:
-        ifname = "gtp-gnb"
+        ifname = "eth1"
         addr = ni.ifaddresses(ifname)
-        sourceIP = "12.1.1.2"
-        print(sourceIP)
-        if sourceIP in ["12.1.1.2", "12.1.1.3", "12.1.1.4"]:
+        if is_attack == 1:
             pcap_dir = "../ddos-data-sets-2022/benign_traffic"
             pcap_files = os.listdir(os.path.join(pcap_dir))
             if not pcap_files:
                 print(f"No pcap files found for traffic type. Skipping...")
-                continue
+                
         else: 
-            pcap_dir = "../malicious"
+            pcap_dir = "ddos-data-sets-2022/attack_traffic"
             pcap_files = os.listdir(os.path.join(pcap_dir))
             if not pcap_files:
                 print(f"No pcap files found for traffic type. Skipping...")
-                continue
-
-
-
+                
     except:
         pass
     #exit()
-
-        for pcap_file in pcap_files:
-            
-            full_pcap_path = os.path.join(pcap_dir, pcap_file)
-            print(full_pcap_path)
-            traffic_generator = Replay_Traffic(sourceIP, destinationIP,oldSource,full_pcap_path)
-            traffic_generator.send_packet()
-            threads.append(traffic_generator)
+    print("before the for")
+    for pcap_file in pcap_files:
+        print("in the for")
+        full_pcap_path = os.path.join(pcap_dir, pcap_file)
+        print(full_pcap_path)
+        traffic_generator = Replay_Traffic(sourceIP, destinationIP,full_pcap_path)
+        traffic_generator.send_packet(sourceIP, destinationIP,full_pcap_path)
+        threads.append(traffic_generator)
 
     # Wait for all threads to complete
     for thread in threads:
